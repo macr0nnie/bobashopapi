6d5675b236c0145e9e184129a2ee135dbfc3a955
macronnie <macr0nnieasmr@gmail.com>
2025-04-09 15:36:03 -0400

    removed junk files

--- obj/Debug/net9.0/BobaShopApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BobaShopApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50cef4bbc64c79a52228eaea2da907f93e56856e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b8249883daa036e1377b81a8de44dec763ba4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
