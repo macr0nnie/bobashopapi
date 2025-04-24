1f8bde1f34e5fe251b9cb8ecfd6c0d4933235bab
macronnie <macr0nnieasmr@gmail.com>
2025-04-24 16:15:25 -0400

    cool

--- obj/Debug/net9.0/BobaShopApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BobaShopApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd9f587cf88904d38854579cdb7391665fb41a4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a93e7d56c3016367ef2e7788e7e8bbb1fe2bb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
