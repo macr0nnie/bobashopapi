10353847d5484eb3cbed7357b1ad7130cd08fc9d
macronnie <macr0nnieasmr@gmail.com>
2025-04-30 12:43:45 -0400

    removed data that didnt have urls

--- Data/BobaShopContext.cs
+++ Data/BobaShopContext.cs
@@ -15,12 +15,6 @@ namespace BobaShopApi.Data
             base.OnModelCreating(modelBuilder); //what is this for?
             //seed data
             modelBuilder.Entity<Drink>().HasData(
-                new Drink { Id = 1, Name = "Classic Milk Tea", Price = 3.50m },
-                new Drink { Id = 2, Name = "Taro Milk Tea", Price = 4.00m },
-                new Drink { Id = 3, Name = "Matcha Latte", Price = 4.50m },
-                new Drink { Id = 4, Name = "Mango Smoothie", Price = 5.00m },
-                new Drink { Id = 5, Name = "Strawberry Lemonade", Price = 3.75m },
-                new Drink { Id = 6, Name = "The blood of my enemies", Price = 4.25m },
                 new Drink { Id = 7, Name = "Vegan Juice", Price = 20.8m, ImageUrl = "https://example.com/vegan-juice.jpg" },
                 new Drink { Id = 8, Name = "Peach Oolong Tea", Price = 3.25m ,ImageUrl  = "https://www.bing.com/th/id/OIP.Gsl2sxyGt020l_KzrKqEHAHaFE?w=273&h=211&c=8&rs=1&qlt=90&o=6&dpr=2&pid=3.1&rm=2"}
             );
