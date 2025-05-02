ed30e13e6c44a6a65639e44c562a01027027602e
macronnie <macr0nnieasmr@gmail.com>
2025-05-02 14:44:39 -0400

    Revert "new change ig"
    This reverts commit 079aa9d19faf6dae5f8cae4e25a3d39a89f1e432.

--- Migrations/20250501152133_rename_stored_procedure.cs
+++ Migrations/20250501152133_rename_stored_procedure.cs
@@ -19,9 +19,7 @@ namespace BobaShopApi.Migrations
             @Salary DECIMAL(18, 2) = NULL,
             @Shift NVARCHAR(MAX) = NULL,
             @SortColumn NVARCHAR(MAX) = NULL,
-            @SortDirection NVARCHAR(MAX) = NULL,
-            @MinSalary DECIMAL(18, 2) = NULL,
-            @MaxSalary DECIMAL(18, 2) = NULL
+            @SortDirection NVARCHAR(MAX) = NULL
 
             AS
             BEGIN 
@@ -32,12 +30,11 @@ namespace BobaShopApi.Migrations
                   (@Name IS NULL OR Name LIKE '%' + @Name + '%') AND
                   (@Position IS NULL OR Position LIKE '%' + @Position + '%') AND
                   (@Salary IS NULL OR Salary = @Salary) AND
-                  (@Shift IS NULL OR Shift LIKE '%' + @Shift + '%') AND
-                  (@MinSalary IS NULL OR Salary >= @MinSalary) AND
-                  (@MaxSalary IS NULL OR Salary <= @MaxSalary)
+                  (@Shift IS NULL OR Shift LIKE '%' + @Shift + '%')
 
             END
             ");
+
         }
 
         /// <inheritdoc />
--- obj/Debug/net9.0/BobaShopApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BobaShopApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08fdfc98dd7fc8a7ffe6be85776b7f7fab307ab2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10353847d5484eb3cbed7357b1ad7130cd08fc9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
