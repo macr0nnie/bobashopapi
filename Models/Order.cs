0c668735712c882ad47bbfa68d214dcebd3534c0
macronnie <macr0nnieasmr@gmail.com>
2025-05-05 13:34:59 -0400

    changes

--- Models/Order.cs
+++ Models/Order.cs
@@ -9,5 +9,6 @@ namespace BobaShopApi.Models
         public string? CustomerName { get; set; }
         public decimal TotalAmount { get; set; }
         public DateTime OrderDate { get; set; }
+        
     }
 }
