41496b7181f9674278156387e9be27dc702f4e5d
macronnie <macr0nnieasmr@gmail.com>
2025-05-06 10:40:31 -0400

    oops

--- obj/Debug/net9.0/BobaShopApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BobaShopApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d2d5ebfa5f5b9b62ae9876a9a3884157784a168")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c76e886084c99ced8b6ff842a7214c54d9224009")]
 [assembly: System.Reflection.AssemblyProductAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
