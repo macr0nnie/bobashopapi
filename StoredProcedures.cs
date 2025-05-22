f4719beb2480dc0e89c8132ee2332c3ab58bb295
macronnie <macr0nnieasmr@gmail.com>
2025-05-22 15:11:20 -0400

    fixed the build errors

--- StoredProcedures.cs
+++ StoredProcedures.cs
@@ -1,32 +1,34 @@
 
 namespace BobaShopApi
 {
-public static class StoredProcedures {
-    //order class stored procedures
-    public const string GetCustomerOrders = "get_customer_orders";
-    public const string UpdateOrderStatus = "update_order_status";
-    public const string GetProductDetails = "get_product_details";
-    public const string ArchiveOldOrders = "archive_old_orders";
+    public static class StoredProcedures
+    {
+        //order class stored procedures
+        public const string GetCustomerOrders = "get_customer_orders";
+        public const string UpdateOrderStatus = "update_order_status";
+        public const string GetProductDetails = "get_product_details";
+        public const string ArchiveOldOrders = "archive_old_orders";
 
-    //employee stored procedures
-    public const string GetAllEmployees = "get_all_employees";
-    public const string GetEmployeeById = "get_employee_by_id";
-    public const string AddEmployee = "add_employee";
-    public const string UpdateEmployee = "update_employee";
-    public const string DeleteEmployee = "delete_employee";
-    public const string GetFilteredEmployees = "get_filtered_employees";
-    //drink stored procedures
-    public const string GetAllDrinks = "get_all_drinks";
-    public const string GetDrinkById = "get_drink_by_id";
-    public const string AddDrink = "add_drink";
-    public const string UpdateDrink = "update_drink";
-    public const string DeleteDrink = "delete_drink";
-    //order stored procedures
-    public const string GetAllOrders = "get_all_orders";
-    public const string GetOrderById = "get_order_by_id";
-    public const string AddOrder = "add_order";
-    public const string UpdateOrder = "update_order";
-    public const string DeleteOrder = "delete_order";
-    public const string GetFilteredOrders = "get_filtered_orders";
-    public const string GetOrderDetails = "get_order_details";
+        //employee stored procedures
+        public const string GetAllEmployees = "get_all_employees";
+        public const string GetEmployeeById = "get_employee_by_id";
+        public const string AddEmployee = "add_employee";
+        public const string UpdateEmployee = "update_employee";
+        public const string DeleteEmployee = "delete_employee";
+        public const string GetFilteredEmployees = "get_filtered_employees";
+        //drink stored procedures
+        public const string GetAllDrinks = "get_all_drinks";
+        public const string GetDrinkById = "get_drink_by_id";
+        public const string AddDrink = "add_drink";
+        public const string UpdateDrink = "update_drink";
+        public const string DeleteDrink = "delete_drink";
+        //order stored procedures
+        public const string GetAllOrders = "get_all_orders";
+        public const string GetOrderById = "get_order_by_id";
+        public const string AddOrder = "add_order";
+        public const string UpdateOrder = "update_order";
+        public const string DeleteOrder = "delete_order";
+        public const string GetFilteredOrders = "get_filtered_orders";
+        public const string GetOrderDetails = "get_order_details";
+    }
 }
--- obj/Debug/net9.0/BobaShopApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BobaShopApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52a03f5acffff6b12731a32cde5aadcc6af96e27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d49ed48766e8a39e4a64131d33d6a7cad7d0aaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BobaShopApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
